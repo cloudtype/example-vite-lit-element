3e4751c1642f4074e4aa9e78f15040fd3302fdd2
joje <joje.attrs@gmail.com>
2021-06-24 22:01:51 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,9 @@ export default defineConfig({
       formats: ['es']
     },
     rollupOptions: {
-      external: /^lit-element/
+      input: {
+        main: path.join(__dirname, 'index.html')
+      }
     }
   }
 })
